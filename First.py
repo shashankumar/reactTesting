145189ca47adafccdbe643a114596aaa8460006a
shashank <shashank@shashanks-MacBook-Air.local>
2022-03-28 18:20:52 +0530

    init

---
+++ First.py
@@ -0,0 +1,10 @@
+print("shashank")
+thisset = {"shashank","rupam","ragini"}
+print("rupam" in thisset)
+print("yes")
+for x in thisset:
+    print(x)
+thisset2 = {"richa"}
+thisset.update(thisset2)
+thisset.add("nitu")
+print(thisset)
